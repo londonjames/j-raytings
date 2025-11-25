8aa3012a36eba6982067b9d226d1d1e488b203e1
londonjames <raybould@gmail.com>
2025-11-24 19:30:05 -0800

    Update year seen filter to show individual years 2025-2006 plus Pre-2006
    - Change from decade ranges (2020s, 2010s, 2000s, Pre-2000) to individual years
    - Shows years 2025, 2024, 2023, ... 2006, Pre-2006
    - More granular filtering for year watched

--- frontend/src/components/FilterBar.jsx
+++ frontend/src/components/FilterBar.jsx
@@ -199,10 +199,27 @@ function FilterBar({ onFilterChange, onActiveFiltersChange }) {
 
   // Year seen ranges (for year watched)
   const yearSeenRanges = [
-    '2020s',
-    '2010s',
-    '2000s',
-    'Pre-2000'
+    '2025',
+    '2024',
+    '2023',
+    '2022',
+    '2021',
+    '2020',
+    '2019',
+    '2018',
+    '2017',
+    '2016',
+    '2015',
+    '2014',
+    '2013',
+    '2012',
+    '2011',
+    '2010',
+    '2009',
+    '2008',
+    '2007',
+    '2006',
+    'Pre-2006'
   ]
 
   // RT score ranges
